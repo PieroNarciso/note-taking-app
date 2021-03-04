534fe04522885f0c86453dd3c29820eb57f8d2f9
Piero Narciso <pieronarciso988@gmail.com>
2021-03-04 18:59:29 -0500

    Added state reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,32 @@
+const initialState = {
+  posts: [
+    { id: 1, title: "Post number 1", content: "Hello world!", color: "red" },
+    {
+      id: 2,
+      title: "Post number 2",
+      content: "Finish this project",
+      color: "green",
+    },
+    {
+      id: 3,
+      title: "Post number 3",
+      content: "Another example",
+      color: "blue",
+    },
+    { id: 4, title: "Post number 4", content: "Example 3,2,1..." },
+  ],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_POST":
+      return {
+        ...state,
+        posts: state.posts.concat(action.payload)
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
