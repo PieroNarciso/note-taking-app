817593ebbb7da537c861b37b591c808eb3f63012
Piero Narciso <pieronarciso988@gmail.com>
2021-03-04 19:00:04 -0500

    Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const header = () => {
+  return (
+    <header className="px-4 py-4 bg-yellow-600 shadow-lg">
+      <h1 className="text-xl font-semibold text-gray-50">Keeper App</h1>
+    </header>
+  );
+};
+
+export default header;
